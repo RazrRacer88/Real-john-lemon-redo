845fdaf503796d252a1b3769660a7b1c95a5cc80
RzrRacer88 <Georgellamosas@hotmail.com>
2024-01-16 13:55:16 -0800

    2.9

--- John/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ John/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Vector3 m_Movement;
-    
+    Animator m_Animator;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       m_Animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -18,5 +19,13 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+
+        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
     }
 }
